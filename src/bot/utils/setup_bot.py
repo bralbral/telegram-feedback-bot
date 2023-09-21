74bb5e926a9fe4ea4018bd42cd64d985f060a1eb
bbralyc <bbralyc@gmail.com>
2023-09-21 20:17:23 +0400

    bumps readme.md

--- src/bot/utils/setup_bot.py
+++ src/bot/utils/setup_bot.py
@@ -1,16 +1,24 @@
 from aiogram import Bot
+from aiogram.types import BotCommand
+from aiogram.types import BotCommandScopeDefault
 from sulguk import AiogramSulgukMiddleware
 
 from src.config import BotConfig
 
 
-def setup_bot(config: BotConfig) -> Bot:
+async def setup_bot(config: BotConfig) -> Bot:
     bot: Bot = Bot(
         token=config.token.get_secret_value(),
     )
 
     # https://github.com/Tishka17/sulguk#example-for-aiogram-users
     bot.session.middleware(AiogramSulgukMiddleware())
+
+    user_commands = [
+        BotCommand(command="help", description="How to use bot"),
+    ]
+
+    await bot.set_my_commands(user_commands, scope=BotCommandScopeDefault())
 
     return bot
 
